efb5a4687c260b541d064284f1a7359431aec308
Grigoriy <slimbde@gmail.com>
2021-04-15 19:39:25 +0500

    Updates

---
+++ config.js
@@ -0,0 +1,1 @@
+window.tileColors=["lightcoral","limegreen","darkgoldenrod","yellow","brown","lightblue","orange","cornflowerblue"],window.tileSize=50,window.attempts=10,window.goalPoints=1100,window.shuffles=3;
